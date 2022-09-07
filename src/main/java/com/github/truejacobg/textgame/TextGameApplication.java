31581c008abbe51fa8dc2e4f627808c358382f03
Jakub <jakubgradzewicz1309@gmail.com>
2022-09-07 12:30:07 +0200

    start of the project

---
+++ src/main/java/com/github/truejacobg/textgame/TextGameApplication.java
@@ -0,0 +1,13 @@
+package com.github.truejacobg.textgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextGameApplication.class, args);
+    }
+
+}
